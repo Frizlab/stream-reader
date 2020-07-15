53a8dcc7acf5e73ce6382411f8a609ad210e7a5f
Frizlab <fload@me.com>
2020-07-15 17:35:09 +0200

    Remove some details about exception handling in Swift

--- Sources/SimpleStream/Implementations/SimpleFileHandleStream.swift
+++ Sources/SimpleStream/Implementations/SimpleFileHandleStream.swift
@@ -11,50 +11,15 @@ import Foundation
 
 extension FileHandle : GenericReadStream {
 	
-	/* *** Exception handling, adapted from https://stackoverflow.com/a/34958339
-	 * Sadly, this does not actually work. The NSSetUncaughtExceptionHandler is
-	 * meant to allow last-minute logging before everything crashes, but does not
-	 * allow recovering from the exception.
-	 *
-	 * ObjC-exception handling in PURE swift is apparently not possible at all.
-	 *
-	 * (Also not this method changes the GLOBAL uncaught exception handler for
-	 * all objects. We tried limiting the damage by using a queue and properly
-	 * setting the handler only one read at a time, but other code could change
-	 * the handler in parallel, fucking up everything.) */
-//	static var frz_caughtException: NSException?
-//	static var frz_existingHandler: (@convention(c) (NSException) -> Void)?
-//	static var frz_exceptionQueue = DispatchQueue(label: "FileHandle ObjC Exception Catching in Swift")
-//	static func frz_convertObjCExceptionToSwift<T>(in handler: () -> T) throws -> T {
-//		/* We must dispatch on a sync serial queue because changing the uncaught
-//		 * exception handler is a global operation. */
-//		return try frz_exceptionQueue.sync{
-//			assert(FileHandle.frz_existingHandler == nil)
-//			assert(FileHandle.frz_caughtException == nil)
-//
-//			FileHandle.frz_existingHandler = NSGetUncaughtExceptionHandler()
-//			NSSetUncaughtExceptionHandler{ exception in
-//				FileHandle.frz_caughtException = exception
-//			}
-//			defer {
-//				NSSetUncaughtExceptionHandler(FileHandle.frz_existingHandler)
-//				FileHandle.frz_existingHandler = nil
-//				FileHandle.frz_caughtException = nil
-//			}
-//
-//			let ret = handler()
-//			if let exception = FileHandle.frz_caughtException {
-//				throw ObjCExceptionError(exception: exception)
-//			}
-//			return ret
-//		}
-//	}
-	
 	public func read(_ buffer: UnsafeMutableRawPointer, maxLength len: Int) throws -> Int {
 		let data: Data
 		if #available(macOS 10.15.4, iOS 13.4, tvOS 13.4, watchOS 6.2, *) {
 			data = try read(upToCount: len) ?? Data()
 		} else {
+			/* This might throw (an ObjC exception). Sadly these exception are not
+			 * catchable at all in “pure” Swift, so we’re not catching them… Anyway
+			 * this is a deprecated API, and the more modern version is used when
+			 * available. */
 			data = readData(ofLength: len)
 		}
 		let sizeRead = data.count
@@ -67,11 +32,3 @@ extension FileHandle : GenericReadStream {
 }
 
 public typealias SimpleFileHandleStream = SimpleGenericReadStream
-
-
-
-//struct ObjCExceptionError : Error {
-//
-//	var exception: NSException
-//
-//}
