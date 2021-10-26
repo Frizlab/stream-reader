d1a8d91c9d46ce55a1848379a859a09e36e0f9da
Frizlab <fload@me.com>
2021-10-26 12:09:00 +0200

    Add support for URLSessionStreamTask read

---
+++ Sources/StreamReader/Conveniences/URLSessionStreamTaskReader.swift
@@ -0,0 +1,33 @@
+/*
+ * URLSessionStreamTaskReader.swift
+ * StreamReader
+ *
+ * Created by François Lamboley on 26/10/2021.
+ */
+
+import Foundation
+
+
+
+@available(macOS 10.11, *)
+extension URLSessionStreamTask : GenericReadStream {
+	
+	public func read(_ buffer: UnsafeMutableRawPointer, maxLength len: Int) throws -> Int {
+		var data: Data?, error: Error?
+		let group = DispatchGroup()
+		group.enter()
+		readData(ofMinLength: 1, maxLength: len, timeout: .infinity, completionHandler: { d, eof, err in
+			/* We don’t need the EOF value */
+			data = d
+			error = err
+			group.leave()
+		})
+		group.wait()
+		if let e = error {throw e}
+		data?.withUnsafeBytes{ bytes in buffer.copyMemory(from: bytes.baseAddress!, byteCount: bytes.count) }
+		return data?.count ?? 0
+	}
+	
+}
+
+public typealias URLSessionStreamTaskReader = GenericStreamReader
