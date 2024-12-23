e9a4d0df88b01a84fe1d66df8982eeb2070f2416
Frizlab <fload@me.com>
2024-12-23 12:36:23 +0100

    Fix concurrency warnings

--- Sources/StreamReader/Conveniences/URLSessionStreamTaskReader.swift
+++ Sources/StreamReader/Conveniences/URLSessionStreamTaskReader.swift
@@ -16,19 +16,24 @@ import Foundation
 extension URLSessionStreamTask : GenericReadStream {
 	
 	public func read(_ buffer: UnsafeMutableRawPointer, maxLength len: Int) throws -> Int {
-		var data: Data?, error: Error?
+		let readResults = ReadResults()
 		let group = DispatchGroup()
 		group.enter()
-		readData(ofMinLength: 1, maxLength: len, timeout: .infinity, completionHandler: { d, eof, err in
+		readData(ofMinLength: 1, maxLength: len, timeout: .infinity, completionHandler: { d, atEOF, err in
 			/* We don’t need the EOF value. */
-			data = d
-			error = err
+			readResults.data = d
+			readResults.error = err
 			group.leave()
 		})
 		group.wait()
 		if let e = error {throw e}
-		data?.withUnsafeBytes{ bytes in buffer.copyMemory(from: bytes.baseAddress!, byteCount: bytes.count) }
-		return data?.count ?? 0
+		readResults.data?.withUnsafeBytes{ bytes in buffer.copyMemory(from: bytes.baseAddress!, byteCount: bytes.count) }
+		return readResults.data?.count ?? 0
+	}
+	
+	/* This is not Sendable at all, but we guarantee we’ll use it responsibly. */
+	private final class ReadResults : @unchecked Sendable {
+		var data: Data?, error: Error?
 	}
 	
 }
